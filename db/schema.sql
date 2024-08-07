e29e3a30fe21c91f61985a84377906c19f318174
geovko <octobermes@gmail.com>
2024-08-06 21:31:31 -0700

    added the main db structures

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,30 @@
-DROP DATABASE IF EXISTS staff_db;
-CREATE DATABASE staff_db;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
-\c staff_db;
+\c company_db;
 
-CREATE TABLE favorite_books (
+CREATE TABLE departments (
   id SERIAL PRIMARY KEY,
-  section INTEGER NOT NULL,
-  book_name VARCHAR(30) NOT NULL,
-  in_stock BOOLEAN,
-  quantity INTEGER NOT NULL
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE roles (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id),  
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(id)
 );
